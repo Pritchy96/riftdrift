59175066a73c49503509504a1c805f6bc1da46a9
Tom Pritchard <thomas.pritchard-2@student.manchester.ac.uk>
2016-03-07 22:57:16 +0000

    Intial Commit

---
+++ server.js
@@ -0,0 +1,30 @@
+// Libraries
+var express = require('express');
+var morgan  = require('morgan');
+var exec = require('child_process').exec;
+
+// Create the server instance
+var app = express();
+
+// Report connections to console
+app.use(morgan('dev'));
+
+// Serve the web content
+app.use(express.static('public_html'));
+
+// Converts a serialized location string into a object
+function convertLocation(location) {
+  location = location.substring(1, location.length-2);
+  var elements = location.replace(/\s+/g, " ").split(" ");
+  var x = parseFloat(elements[0]);
+  var y = parseFloat(elements[1]);
+  var z = parseFloat(elements[2]);
+  return {"x":x, "y": y, "z": z}
+}
+
+// Start the server
+var server = app.listen(80, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Example app listening at http://%s:%s", host, port)
+});
