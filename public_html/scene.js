917a01ed5d280cee24f66621bd007e1236f70676
Tom Pritchard <thomas.pritchard-2@student.manchester.ac.uk>
2016-03-08 01:40:21 +0000

    Initial Commit of base code

--- public_html/scene.js
+++ public_html/scene.js
@@ -1,56 +1,120 @@
-// Setup three.js WebGL renderer. Note: Antialiasing is a big performance hit.
-// Only enable it if you actually need to.
-var renderer = new THREE.WebGLRenderer({antialias: false});
-renderer.setPixelRatio(window.devicePixelRatio);
+// Camera view variables
+var cameraMode = "DEFAULT";
+var target = null;
 
-// Append the canvas element created by the renderer to document body element.
-document.body.appendChild(renderer.domElement);
+// OpenGL dark magic
+var scene = null;
+var camera = null;
+var controls = null;
 
-// Create a three.js scene.
-var scene = new THREE.Scene();
+// Invoked when the window loads and the app can be started
+$(window).load(function() {
+  //Pre scene creation code here.
 
-// Create a three.js camera.
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+  // Call the main function to start the app, when window is loaded.
+  main();
+});
 
-// Apply VR headset positional data to camera.
-var controls = new THREE.VRControls(camera);
+  // Move the camera to face the planet
+  updateView();
 
-// Apply VR stereo rendering to renderer.
-var effect = new THREE.VREffect(renderer);
-effect.setSize(window.innerWidth, window.innerHeight);
+// Updates the view of the camera
+function updateView() {
+  console.log("updateView has been invoked.");
 
 
-// Create a VR manager helper to enter and exit VR mode.
-var params = {
-  hideButton: false, // Default: false.
+  // If there is a target planet then look at the target planet
+  if (target != null) {
+    // The normalized angle
+    var matrix = new THREE.Matrix4();
+    matrix.extractRotation(camera.matrix);
 
-  isUndistorted: false // Default: false.
-};
-var manager = new WebVRManager(renderer, effect, params);
+    var direction = new THREE.Vector3(0, 0, 1);
+    matrix.multiplyVector3(direction);
+    // normalized angle
+    var newCameraPos = target.position.clone().add(direction.multiplyScalar(0.8*Math.pow(target.diameter,0.8)));
+    console.log(newCameraPos);
 
-// Request animation frame loop function
-var lastRender = 0;
-function animate(timestamp) {
-  var delta = Math.min(timestamp - lastRender, 500);
-  lastRender = timestamp;
-
-  // Update VR headset position and apply to camera.
-  controls.update();  
-
-  // Render the scene through the manager.
-  manager.render(scene, camera, timestamp);
-
-  requestAnimationFrame(animate);
-}
-
-// Kick off animation loop
-animate(performance ? performance.now() : Date.now());
-
-// Reset the position sensor when 'z' pressed.
-function onKey(event) {
-  if (event.keyCode == 90) { // z
-    controls.resetSensor();
+    // Set the position of the camera to be by the planet
+    camera.position.set(newCameraPos.x, newCameraPos.y, newCameraPos.z);
   }
 }
 
+// The main entry point for the application that gets calleddto start rendering things and other GL stuff.
+function main() {
+  // Setup three.js WebGL renderer. Note: Antialiasing is a big performance hit.
+  // Only enable it if you actually need to.
+  var renderer = new THREE.WebGLRenderer({antialias: false});
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  // Append the canvas element created by the renderer to document body element.
+  document.body.appendChild(renderer.domElement);
+
+  // Create the camera.
+  camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 10000);
+  camera.position.x = 0;
+  camera.position.y = 0;
+  camera.position.z = 0;
+
+  // Call updateView of the first time so the camera is in the correct position
+  updateView();
+
+  // Apply VR headset positional data to camera.
+  controls = new THREE.VRControls(camera);
+
+  // Apply VR stereo rendering to renderer.
+  var effect = new THREE.VREffect(renderer);
+  effect.setSize(window.innerWidth, window.innerHeight);
+
+  // Create a VR manager helper to enter and exit VR mode.
+  var params = { hideButton: false, isUndistorted: false };
+  var manager = new WebVRManager(renderer, effect, params);
+
+  // Animation loop
+  var lastRender = 0;
+
+  // Create a three.js scene.
+  var scene = new THREE.Scene();
+  scene.add( new THREE.AmbientLight(0x444444));
+
+  //Skybox.
+  var geometry = new THREE.SphereGeometry(9000, 60, 40);
+  var material = new THREE.MeshPhongMaterial( { map: THREE.ImageUtils.loadTexture('asset_src/skySphere.jpg') } );
+  skyBox = new THREE.Mesh(geometry, material);
+  skyBox.scale.set(-1, 1, 1);  //Flip so it's internally textured.
+  skyBox.eulerOrder = 'XZY';
+  skyBox.renderDepth = 1000.0;
+  scene.add(skyBox);
+
+  // Create a VR manager helper to enter and exit VR mode.
+  var params = {
+    hideButton: false, // Default: false.
+    isUndistorted: false // Default: false.
+  };
+
+  var manager = new WebVRManager(renderer, effect, params);
+
+  // Request animation frame loop function
+  var lastRender = 0;
+  function animate(timestamp) {
+    var delta = Math.min(timestamp - lastRender, 500);
+    lastRender = timestamp;
+
+    controls.update();
+
+    // Render the scene through the manager.
+    manager.render(scene, camera, timestamp);
+    requestAnimationFrame(animate);
+  }
+// Kick off animation loop
+animate(performance ? performance.now() : Date.now());
+}
+
+// Function that gets called whenever a key is pressed down
+function onKey(event) {
+  // key ','
+  if (event.keyCode == 90 /*z*/){
+    controls.zeroSensor();
+  }
+}
 window.addEventListener('keydown', onKey, true);
--- server.js
+++ server.js
@@ -14,12 +14,24 @@ app.use(express.static('public_html'));
 
 // Converts a serialized location string into a object
 function convertLocation(location) {
-  location = location.substring(1, location.length-2);
-  var elements = location.replace(/\s+/g, " ").split(" ");
-  var x = parseFloat(elements[0]);
-  var y = parseFloat(elements[1]);
-  var z = parseFloat(elements[2]);
-  return {"x":x, "y": y, "z": z}
+	location = location.substring(1, location.length-2);
+	var elements = location.replace(/\s+/g, " ").split(" ");
+	var x = parseFloat(elements[0]);
+	var y = parseFloat(elements[1]);
+	var z = parseFloat(elements[2]);
+	return {"x":x, "y": y, "z": z}
+}
+
+// Allow the user to specify their own date for the planet data
+app.get('/api/planet_data/:year/:month/:day/:hour', function(req, res) {
+	res.set({ 'Content-Type': 'application/json' });
+	sendPlanetJSON(res, req, req.params.year, req.params.month, req.params.day, req.params.hour);
+});
+
+function sendPlanetJSON(res, req, Year, Month, Day, Hour) {
+	var apiCall = "python script/better_skyfield.py " + Year + " " + Month + " " + Day + " " + Hour; 
+	var json;
+ 	exec(apiCall, function(error, stdout, stderr) { res.end(stdout); });
 }
 
 // Start the server
